aaa02d95d81c7c5d6f75ab52cc078b217fbcac96
ajaydewangan1100 <ajaydewangan1100@gmail.com>
2023-09-06 08:56:15 +0530

    render in group and also using skip and only under greet.test.ts

--- src/components/greet/greet.test.tsx
+++ src/components/greet/greet.test.tsx
@@ -1,8 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import { Greet } from "./Greet";
 
-test("render greet", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+  test.skip("render Hello text", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
+
+  test.only("render Hello with name", () => {
+    render(<Greet name="Ajay" />);
+    const textElement = screen.getByText(/hello ajay/i);
+    expect(textElement).toBeInTheDocument();
+  });
 });
